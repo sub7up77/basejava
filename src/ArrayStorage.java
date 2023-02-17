ccc392c95872dd72d08559cb2fae3e0f191b8bf8
Сергей Иващенко <serege@hotmail.com>
2023-02-17 12:31:01 +0300

    HW_1 | update1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,41 +5,40 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
-        Arrays.fill(storage, 0, size(), null);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        int len = size();
-        if (len < storage.length) storage[len] = r;
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
-        int len = size();
-        if (len != 0) {
-            for (int index = 0; index < len; index++) {
-                if (storage[index].uuid.equals(uuid)) {
-                    Resume r = new Resume();
-                    r.uuid = storage[index].uuid;
-                    return r;
-                }
+        if (size != 0) {
+            for (int index = 0; index < size; index++) {
+                if (storage[index].uuid.equals(uuid)) return storage[index];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int len = size();
-        if (len != 0) {
-            for (int index = 0; index < len; index++) {
+        if (size != 0) {
+            for (int index = 0; index < size; index++) {
                 if (storage[index].uuid.equals(uuid)) {
-                    if (index == len - 1) {
+                    if (index == size - 1) {
                         storage[index] = null;
                     } else {
-                        System.arraycopy(storage, index + 1, storage, index, len - 1 - index);
-                        storage[len - 1] = null;
+                        System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
+                        storage[size - 1] = null;
                     }
+                    size--;
                     return;
                 }
             }
@@ -50,12 +49,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int len = 0;
-        while (len < storage.length && storage[len] != null) len++;
-        return len;
+        return size;
     }
 }
